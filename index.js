631d0d78640abce8bdf622c3580964e65b9d9dc2
unknown <sumairbinkalam20@gmail.com>
2022-10-25 18:44:56 +0600

    some data added

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const cors = require('cors');
 app.use(cors());
 const port = process.env.PORT || 5000;
 
+
+const courses = require('./data/course-catagory.json');
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+const course = require('./data/course.json');
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(c => c.id === id);
+    res.send(selectedCourse)
+    console.log(selectedCourse)
+})
+
 app.get('/', (req, res) => {
     res.send('api server is running')
 });
