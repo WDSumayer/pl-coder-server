62fd4e1c57133798e3c405e3bffe60f08f050688
unknown <sumairbinkalam20@gmail.com>
2022-10-25 10:55:06 +0600

    server creation added

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('api server is running')
+});
+app.listen(port, () => {
+    console.log('port', port)
+})
